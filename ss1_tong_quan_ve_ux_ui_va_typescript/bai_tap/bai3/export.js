06afdec2216fd2b455a1d4cf8ded5b132c9d0d1c
unknown <buivanluu1997@gmail.com>
2024-12-12 16:19:22 +0700

    nộp bài tập

---
+++ ss1_tong_quan_ve_ux_ui_va_typescript/bai_tap/bai3/export.js
@@ -0,0 +1,5 @@
+export const sum = (a, b) => a + b;
+
+export default function subtract (a, b) {
+    return a - b;
+}
---
+++ ss1_tong_quan_ve_ux_ui_va_typescript/bai_tap/test/import.js
@@ -0,0 +1,4 @@
+import {sum} from "../bai3/export.js";
+import subtract from "../bai3/export.js";
+console.log(sum(1,2));
+console.log(subtract(4,2));
