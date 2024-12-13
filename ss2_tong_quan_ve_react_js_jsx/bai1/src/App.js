83a3ed5d79f59572c9315a3ed558e66a22d3ba01
unknown <buivanluu1997@gmail.com>
2024-12-13 11:39:41 +0700

    nộp bài tập

--- ss2_tong_quan_ve_react_js_jsx/bai1/src/App.js
+++ ss2_tong_quan_ve_react_js_jsx/bai1/src/App.js
@@ -2,24 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const phones = [{id: 1, name: "IPhone 15 pro", brand: "Apple", price: 1000},
+      {id: 2, name: "SamSung S21", brand: "SamSung", price: 900},
+      {id: 3, name: "Oppo A7", brand: "Oppo", price: 700}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>List Phone</h1>
+        <table border="1">
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Brand</th>
+                <th>Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            {phones.map((phone) =>(
+                <tr key={phone.id}>
+                    <td>{phone.id}</td>
+                    <td>{phone.name}</td>
+                    <td>{phone.brand}</td>
+                    <td>{phone.price}$</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
     </div>
   );
+
+
+
 }
 
 export default App;
